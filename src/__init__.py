57ee2600eeb1092d95e15024957f1fe844e29aec
Yuanze Chen <chenyz61@hotmail.com>
2018-08-31 15:21:06 +0200

    update pre-processing
    sent+aster+geo data preprocessing is mainly done using ArcGIS, the notebook is to split into tiles

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import AdvSeg
-from .networks import SegmentationNet, AdversarialNet
-from .data import Data, DataGenerator
-from .utils import split2Tiles, split_trn_vld_tst, remove_unknown_rocks, TrainValTensorBoard
