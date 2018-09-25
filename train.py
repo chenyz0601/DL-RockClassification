81b57885b8ffb72922fd6d800e69da753a62eece
Yuanze Chen <yz.chen@tum.de>
2018-09-25 09:32:21 +0200

    from command line

--- train.py
+++ train.py
@@ -3,15 +3,25 @@ from src.DeepRock.data import DataGenerator
 from src.DeepRock.utils import split_trn_vld_tst
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-def train(X_trn, Y_trn, X_vld, Y_vld, n_ch_list=[64, 64, 64, 64], lr=1e-3, epochs=100, batch_size=8, dtype='sent'):
+def train(X_trn, Y_trn, X_vld, Y_vld, n_ch_list=[64, 64, 64, 64], lr=1e-3,
+          epochs=100, batch_size=8, dtype='sent'):
     conv = AdvSeg(dtype=dtype)
     conv.build_SegmentationNet(lr=lr, n_ch_list=n_ch_list)
-    trn_data = DataGenerator(X_trn, Y_trn, batch_size=batch_size, dtype=dtype, intype=conv.model_type, pred_fn=conv.predict)
-    vld_data = DataGenerator(X_vld, Y_vld, batch_size=batch_size, dtype=dtype, intype=conv.model_type, pred_fn=conv.predict)
-    conv.fit_model_generator(trn_data, vld_data, seg_epochs=epochs, seg_steps_per_epoch=None, save_weights=True)
+    trn_data = DataGenerator(X_trn, Y_trn, batch_size=batch_size, dtype=dtype,
+                            intype=conv.model_type, pred_fn=conv.predict)
+    vld_data = DataGenerator(X_vld, Y_vld, batch_size=batch_size, dtype=dtype,
+                             intype=conv.model_type, pred_fn=conv.predict)
+    conv.fit_model_generator(trn_data, vld_data, seg_epochs=epochs,
+                             seg_steps_per_epoch=None, save_weights=True)
+
+def main(dtype):
+    X_trn, Y_trn, X_vld, Y_vld, _, _ = split_trn_vld_tst('./data/train/',
+            vld_rate=0.2, tst_rate=0.1, seed=10)
+    # fine tune the learning rate
+    train(X_trn, Y_trn, X_vld, Y_vld, n_ch_list=[64, 64, 64, 64], lr=5e-4,
+            batch_size=8, epochs=200, dtype=dtype)
 
 if __name__ == '__main__':
-    X_trn, Y_trn, X_vld, Y_vld, _, _ = split_trn_vld_tst('./data/train/', vld_rate=0.2, tst_rate=0.1, seed=10)
-    # fine tune the learning rate
-    train(X_trn, Y_trn, X_vld, Y_vld, n_ch_list=[64, 64, 64, 64], lr=5e-4, batch_size=8, epochs=200, dtype='sent')
+    main(sys.argv[1])
